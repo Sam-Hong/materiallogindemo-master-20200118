f25bd469077f0f94f1a0fb8da2681658ec3bafd3
Kristopher yang <kristopheryang1@gmail.com>
2020-02-21 15:20:48 +0800

    fixed statistics and sorted it too

--- app/src/main/java/com/sourcey/materiallogindemo/statisticsPageActivity.java
+++ app/src/main/java/com/sourcey/materiallogindemo/statisticsPageActivity.java
@@ -22,18 +22,29 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class statisticsPageActivity extends AppCompatActivity {
 
     String authorization;
     RequestQueue requestQueue;
     static final String REQ_TAG = "VACTIVITY";
-    ArrayList<String> idList = new ArrayList<String>();
-    ArrayList<String> nameList = new ArrayList<String>();
-    ArrayList<String> urlList = new ArrayList<String>();
-    ArrayList<String> moreFiles = new ArrayList<String>();
+    private ArrayList<String> nameList = new ArrayList<String>();
+    private ArrayList<String> urlList = new ArrayList<String>();
+    private ArrayList<ArrayList<String>> listOfFiles = new ArrayList<>();
+    int balance = 0;
 
     String parentId;
 
@@ -59,7 +70,8 @@ public class statisticsPageActivity extends AppCompatActivity {
                 String[] extra = new String[1];
                 extra[0] = "none";
                 data.putString("name", nameList.get(position));
-                data.putStringArrayList("files", moreFiles);
+                sortFiles(listOfFiles.get(position - balance));
+                data.putStringArrayList("files", listOfFiles.get(position - balance));
                 data.putStringArray("history", extra);
                 Intent intent = new Intent(statisticsPageActivity.this, RegulationsPageActivity.class);
                 intent.putExtra("data", data);
@@ -96,24 +108,27 @@ public class statisticsPageActivity extends AppCompatActivity {
                                     for (int i = 0; i < array.length(); i++) {
                                         JSONObject jsonObject = array.getJSONObject(i);
                                         String type = jsonObject.getString("showType");
-                                        String id = jsonObject.getString("id");
                                         String name = jsonObject.getString("title");
                                         String url;
-                                        if (type.equals("3"))
+                                        if (jsonObject.getString("id").equals("134110"))
+                                            type = "1";
+                                        if (type.equals("3")) {
+                                            balance++;
                                             url= "https://drive.google.com/gview?embedded=true&url=https://www.cga.gov.tw" + jsonObject.getString("filePath");
-                                        else if (type.equals("2"))
+                                        } else if (type.equals("2")) {
+                                            balance++;
                                             url = "none";
-                                        else {
+                                        } else {
                                             url = "more files";
                                             JSONArray other = jsonObject.getJSONArray("files");
+                                            ArrayList<String> moreFiles = new ArrayList<String>();
                                             for (int x = 0; x < other.length(); x++) {
                                                 JSONObject data = other.getJSONObject(x);
                                                 moreFiles.add(data.getString("name") + ".pdf");
                                                 moreFiles.add(data.getString("filePath"));
                                             }
+                                            listOfFiles.add(moreFiles);
                                         }
-
-                                        idList.add(id);
                                         nameList.add(name);
                                         urlList.add(url);
                                     }
@@ -154,6 +169,49 @@ public class statisticsPageActivity extends AppCompatActivity {
         requestQueue.add(jsonObjectRequest);
     }
 
+    private void sortFiles (ArrayList<String> files)
+    {
+        TreeMap<String,String> sortList = new TreeMap<>();
+        ArrayList<String> tempList = new ArrayList<>();
+
+        for(int x = 0; x < files.size(); x += 2)
+            sortList.put(files.get(x),files.get(x+1));
+        Set tree = sortList.entrySet();
+
+        for (Object o : tree) {
+            Map.Entry me = (Map.Entry) o;
+            tempList.add(me.getKey().toString());
+        }
+        Collections.sort(tempList, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                Pattern p = Pattern.compile("\\d+");
+                Matcher m = p.matcher(o1);
+                Matcher m2 = p.matcher(o2);
+
+                while (m.find() && m2.find())
+                {
+                    if (Integer.parseInt(m.group()) > Integer.parseInt(m2.group())) {
+                        return 1;
+                    }
+                    else if (Integer.parseInt(m.group()) < Integer.parseInt(m2.group())) {
+                        return -1;
+                    }
+                }
+                if (m.hitEnd() && m2.hitEnd())
+                    return 0;
+                else
+                    return 1;
+            }
+        });
+
+        files.clear();
+
+        for (int x = 0; x < tempList.size(); x++) {
+            files.add(tempList.get(x));
+            files.add(sortList.get(tempList.get(x)));
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
